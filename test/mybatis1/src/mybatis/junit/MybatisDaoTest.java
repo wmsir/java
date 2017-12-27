16b58eebd97cbcbedbf21aceb7376fbbd0f6b9cd
wm <5815209@qq.com>
2017-12-27 23:59:52 +0800

    add

---
+++ test/mybatis1/src/mybatis/junit/MybatisDaoTest.java
@@ -0,0 +1,32 @@
+package mybatis.junit;
+
+import java.io.InputStream;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import mybatis.dao.UserDao;
+import mybatis.dao.UserDaoImpl;
+import mybatis.pojo.User;
+
+public class MybatisDaoTest {
+
+  public SqlSessionFactory sqlSessionFactory;
+  @Before
+  public void before() throws Exception {
+      //加载核心配置文件
+      String resource = "sqlMapConfig.xml";
+      InputStream in = Resources.getResourceAsStream(resource);
+      //创建SqlSessionFactory
+      sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
+  }
+  @Test
+  public void testDao() throws Exception {
+      
+      UserDao userDao = new UserDaoImpl(sqlSessionFactory);
+      
+      User user = userDao.selectUserById(10);
+      System.out.println(user);
+  }
+}
